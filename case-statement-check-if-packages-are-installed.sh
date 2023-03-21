71a4a294e8a25ae2b351e37ac1cd10c6c05c9fca
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2023-03-20 22:58:11 -0400

    Update case-statement-check-if-packages-are-installed.sh

--- case-statement-check-if-packages-are-installed.sh
+++ case-statement-check-if-packages-are-installed.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+#welcome message
+
 echo "Welcome to the Package checker";
 echo  "Please insert Package name";
 
 read -p '--> ' package ;
 
-#This command will test if the package is installed on the computer
+#This command will test if the package is installed on the computer at the moment
 test=$(dpkg-query -W -f='${Status}\n' $package)
 
 
-#In this part if the package exist 
+#If the package exist you will get an answer about the specific package
 case $test  in
 	"install ok installed") echo "Package is currently installed on the computer";;
 	*) echo "Package is not currently installed or does not exist";;
