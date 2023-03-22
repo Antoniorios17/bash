222cc9b0cb5fb3c0b182de68e4f9c05c26ab3897
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2023-03-22 14:45:09 -0400

    Update if-operator-test.sh

--- if-operator-test.sh
+++ if-operator-test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#The scrip will let you compare if the input is smaller or greater than 5
+#The current version of the script only allows for integers to be used
 
 #This script is an example of using an if then and elif operators.
 
 var1=$1
 
 #Below we will use the -z to check if the variable is empty. Then we will use > and = inside the double brackets.
-
+#
 if [[ -z $var1 ]];
 then
 	echo "Please enter a parameter"
