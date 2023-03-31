eac4e7258576e025c2a1a7664050e33e36ec90dc
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2023-03-30 23:58:52 -0400

    Update ssh-server-ping-test.sh

--- ssh-server-ping-test.sh
+++ ssh-server-ping-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#This is interactive script
+#The reply will let you know that the computer is up or down
 
 ping -c1 -W1 3.82.96.71 && echo 'Server is up' || echo 'Server is down';
 
